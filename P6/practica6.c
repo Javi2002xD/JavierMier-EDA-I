dc206e749ec4ad21b48b50662bb3fd02c5933b71
Javi2002xD <92038114+Javi2002xD@users.noreply.github.com>
2023-04-24 12:48:53 -0600

    Add files via upload

--- P6/practica6.c
+++ P6/practica6.c
@@ -1,1 +1,36 @@
+/*UNIVERSIDAD NACIONAL AUTÓNOMA DE MÉXICO
+ESTRUCTURA DE DATOS Y ALGORITMOS
+MIER RAMÍREZ JAVIER*/
 
+#include <stdio.h>
+#include <string.h>
+
+size_t palindromo(int longitud);
+char palabra[100];
+int longitud, es_palindromo = 1;
+
+int main() {
+
+    printf("\n\n\tIntroduce una palabra: ");
+    scanf("%s", palabra);
+
+    longitud = strlen(palabra);
+    palindromo(longitud);
+
+    
+}
+
+size_t palindromo(int longitud){
+
+    for(int i = 0; i < longitud / 2; i++) {
+        if(palabra[i] != palabra[longitud-i-1]) {
+            es_palindromo = 0;
+            break;
+        }
+    }
+     if(es_palindromo) {
+        return(printf("\t%s es un palindromo.\n\n", palabra));
+    } else {
+        return(printf("\t%s no es un palindromo.\n\n", palabra));
+    }
+}
